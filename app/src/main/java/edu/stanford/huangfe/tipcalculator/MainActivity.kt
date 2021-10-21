9a2013ce24485955b5c695070372bd4e3b3f158e
Felicity H <felicityh15@gmail.com>
2021-10-20 18:47:06 -0700

    first commit:

---
+++ app/src/main/java/edu/stanford/huangfe/tipcalculator/MainActivity.kt
@@ -0,0 +1,100 @@
+package edu.stanford.huangfe.tipcalculator
+
+import android.animation.ArgbEvaluator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+private const val TAG = "MainActviity"
+private const val INITIAL_TIP_PERCENT = 15
+class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmt: EditText
+    private lateinit var seekBarTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmt: TextView
+    private lateinit var tvTotalAmt: TextView
+    private lateinit var tvTipDescription: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etBaseAmt = findViewById(R.id.etBaseAmt)
+        seekBarTip = findViewById(R.id.seekBarTip)
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
+        tvTipAmt = findViewById(R.id.tvTipAmt)
+        tvTotalAmt = findViewById(R.id.tvTotalAmt)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
+
+        updateTipDescription(INITIAL_TIP_PERCENT)
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, "onProgressChanged $progress")
+                tvTipPercentLabel.text = "$progress%"
+                computeTipAndTotal()
+                updateTipDescription(progress)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+        etBaseAmt.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChanged $p0")
+                computeTipAndTotal()
+            }
+
+        })
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent) { //similar to switch in Java
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great"
+            else -> "Amazing"
+        }
+        tvTipDescription.text = tipDescription
+
+        //Update color based on tipPercent
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / seekBarTip.max,
+            ContextCompat.getColor(this, R.color.purple_200),
+            ContextCompat.getColor(this, R.color.teal_200)
+        ) as Int
+        tvTipDescription.setTextColor(color)
+    }
+
+    private fun computeTipAndTotal() {
+        // check if empty string, do early return and show empty text
+        if (etBaseAmt.text.isEmpty()) {
+            tvTipAmt.text = ""
+            tvTotalAmt.text = ""
+            return
+        }
+        // 1. Get value of base and tip percent
+        val baseAmt = etBaseAmt.text.toString().toDouble()
+        val tipPercent = seekBarTip.progress
+        // 2. Compute the tip and total
+        val tipAmt = baseAmt * tipPercent / 100
+        val totalAmt = baseAmt + tipAmt
+        // 3. Update the UI (formatted to only 2 decimal points)
+        tvTipAmt.text = "%.2f".format(tipAmt)
+        tvTotalAmt.text = "%.2f".format(totalAmt)
+
+        //tvTipAmt.text = tipAmt.toString()
+        //tvTotalAmt.text = totalAmt.toString()
+    }
+}
